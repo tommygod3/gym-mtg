5dd41e6639cf819e4b59a494eb71b30a7c4b3041
tommygod3 <tommygod1@live.com>
2020-03-27 23:14:49 +0000

    Changed to player system

--- gym_mtg/envs/mtg_env.py
+++ gym_mtg/envs/mtg_env.py
@@ -15,38 +15,38 @@ class MtgEnv(gym.Env):
         self.players = [player.Player(self.gold_deck), player.Player(self.silver_deck)]
         self.env = game.Game(self.players)
         self.env.start_game()
+        self.current_player = 0
     
     @property
-    def action_space(self, player):
-        if player == self.env.player_with_priority.index:
+    def action_space(self):
+        if self.current_player == self.env.player_with_priority.index:
             legal_moves = self.env.get_legal_moves(self.env.player_with_priority)
             return spaces.Discrete(len(legal_moves))
         else:
             return spaces.Discrete(1)
 
     @property
-    def observation_space(self, player):
-        player_object = self.env.players[player]
+    def observation_space(self):
+        player_object = self.env.players[self.current_player]
         player_decklist = copy.deepcopy(player_object.deck)
-        player_decklist.sort()
 
         return spaces.Dict({
-            "life": len(player_object.life),
-            "hand": len(player_object.hand),
-            "graveyard": len(player_object.graveyard),
-            "deck": len(player_decklist),
-            "battlefield": len(self.env.battlefield),
-            "attackers": len(self.env.attackers),
-            "blockers": len(self.env.blockers),
-            "empty_stack": 1,
-            "damage_targets": len(self.env.damage_targets),
-            "active_player": 1,
-            "nonactive_player": 1,
-            "priority": 1,
-            "current_phase": 1
+            "life": spaces.Discrete(1),
+            "hand": spaces.Discrete(len(player_object.hand)),
+            "graveyard": spaces.Discrete(len(player_object.graveyard)),
+            "deck": spaces.Discrete(len(player_decklist)),
+            "battlefield": spaces.Discrete(len(self.env.battlefield)),
+            "attackers": spaces.Discrete(len(self.env.attackers)),
+            "blockers": spaces.Discrete(len(self.env.blockers)),
+            "empty_stack": spaces.Discrete(1),
+            "damage_targets": spaces.Discrete(len(self.env.damage_targets)),
+            "active_player": spaces.Discrete(1),
+            "nonactive_player": spaces.Discrete(1),
+            "priority": spaces.Discrete(1),
+            "current_phase": spaces.Discrete(1)
         })
 
-    def step(self, action, player):
+    def step(self, action):
         """
 
         Parameters
@@ -77,13 +77,15 @@ class MtgEnv(gym.Env):
                  use this for learning.
         """
 
-        self._take_action(action, player)
-        reward = self._get_reward(player)
-        ob = self.env.get_state(player)
+        self._take_action(action)
+        reward = self._get_reward()
+        ob = self.env.get_state(self.current_player)
         episode_over = self.env.is_over()
+        self.current_player = 1 - self.current_player
         return ob, reward, episode_over, {}
 
     def reset(self):
+        self.current_player = 0
         self.players = [player.Player(self.gold_deck), player.Player(self.silver_deck)]
         self.env = game.Game(self.players)
         self.env.start_game()
@@ -94,15 +96,15 @@ class MtgEnv(gym.Env):
         print("Player 1:")
         print(self.env.get_state(1))
 
-    def _take_action(self, action, player):
-        if player == self.env.player_with_priority.index:
+    def _take_action(self, action):
+        if self.current_player == self.env.player_with_priority.index:
             legal_moves = self.env.get_legal_moves(self.env.player_with_priority)
             self.env.make_move(legal_moves[action], False)
 
-    def _get_reward(self, player):
-        if not self.env.players[player].has_lost:
+    def _get_reward(self):
+        if not self.env.players[self.current_player].has_lost:
             return 1
-        elif self.env.players[1 - player].has_lost:
+        elif self.env.players[1 - self.current_player].has_lost:
             return 100
         else:
             return 0
